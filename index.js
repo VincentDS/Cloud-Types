8882b5a89c293b47b77228328440453dfc4d201b
VincentDS <de_vinnie111_rulezzz@hotmail.com>
2015-08-12 21:39:40 +0200

    updated index

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-CloudTypes = require('./server/Server');
-
-
-var server = CloudTypes.Server;
-
-var groceries = new CloudTypes.Index([{name: 'string'}], {toBuy: 'CInt', country: 'CString'});
-server.declare('groceries', groceries);
-
-var apples = groceries.get('apples');
-
-var toBuy = apples.get('toBuy');
-
-server.start();
-
+exports.Server = require('./server/Server.js');
+exports.Client = require('./client/Client.js');
 
 
